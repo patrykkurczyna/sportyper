a1f2a9de7b6deaff65adc1bf62ebe2930f8a41f9
patrykkurczyna <patryk.kurczyna@schibsted.pl>
2017-05-20 22:24:55 +0200

    Add server and database

--- src/main/java/pl/kurczyna/sportyper/SporTyperApplication.java
+++ src/main/java/pl/kurczyna/sportyper/SporTyperApplication.java
@@ -1,3 +1,5 @@
+package pl.kurczyna.sportyper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/test/groovy/pl/kurczyna/sportyper/SportTyperApplicationTest.groovy
@@ -0,0 +1,11 @@
+package pl.kurczyna.sportyper
+
+import spock.lang.Specification
+
+class SportTyperApplicationTest extends Specification {
+
+    def "should run test"() {
+        expect:
+        2 + 2 == 4
+    }
+}
